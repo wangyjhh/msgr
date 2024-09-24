e460093eab9636fbebf6f407c42bfca7dfd9084b
hikaru <827394320@qq.com>
2024-09-24 14:34:06 +0800

    🐳 chore: 更新测试文件，测试获取用户目录功能

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,19 @@
-import { logf, logv, getEndpoint } from "../utils"
+import { logf, logv, getEndpoint, Client } from "../utils"
+import { homedir } from "node:os"
 
 logf("提示信息", "success", "success")
 logf("提示信息", "warning", "warning")
 logf("提示信息", "error", "error")
 
-console.log(logv("1.0.1"))
+// console.log(logv("1.0.1"))
 
 console.log(getEndpoint("cn-beijing"))
+
+// Client.getSecurityGroup(getEndpoint("cn-beijing"), {
+// 	regionId: "cn-beijing",
+// 	securityGroupId: "sg-2ze7c4gtry9mfi2uwpab",
+// }).then((res) => {
+// 	console.log(res)
+// })
+
+console.log(homedir())
