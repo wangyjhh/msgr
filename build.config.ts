29486bfa3d976e0eb94c41587b77d9a24ba5db45
hikaru <827394320@qq.com>
2024-09-24 14:34:44 +0800

    🐳 chore: 更新构建配置和脚本，优化项目构建流程

--- build.config.ts
+++ build.config.ts
@@ -3,4 +3,10 @@ import { defineBuildConfig } from "unbuild"
 export default defineBuildConfig({
 	entries: ["bin/msgr/index"],
 	clean: true,
+	rollup: {
+		emitCJS: true,
+		esbuild: {
+			minify: true,
+		},
+	},
 })
