b644a83060b7dc57ab3b6ed8246da7fc4de16ea2
hikaru <827394320@qq.com>
2024-10-13 10:24:43 +0800

    🐳 chore: 配置eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+    {
+        stylistic: {
+            indent: 4,
+        },
+        rules: {
+            'antfu/top-level-function': 'off',
+            // 'no-console': 'error',
+        },
+    },
+)
